45d54421ddc36616659e4d6b1a0c245159160998
wayne <event1329211@gamil.com>
2025-05-09 21:42:15 +0800

    添加靜態文件用於GitHub Pages

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Guitar/', // 替換為您的GitHub儲存庫名稱
-})
